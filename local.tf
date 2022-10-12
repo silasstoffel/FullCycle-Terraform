4285db6f5eb6d2cb34b51d6e04353fc958567f3a
Silas Stoffel <silasstofel@gmail.com>
2022-10-12 18:09:14 -0300

    feat: working with datasource

--- local.tf
+++ local.tf
@@ -1,12 +1,17 @@
 resource "local_file" "heroes" {
-  filename = "./tmp/heroes.txt"
+  filename = var.heroes_filename
   content = var.heroes_content
 }
 
 variable "heroes_content" {
   type = string
-  default = "Heros content"
+  default = "Heroes content"
   description = "The heroes content"
+}
+
+variable "heroes_filename" {
+  type = string
+  description = "The heroes filename"
 }
 
 output "heroes_filename_id" {
@@ -15,4 +20,12 @@ output "heroes_filename_id" {
 
 output "heroes_filename_content" {
   value = local_file.heroes.content
+}
+
+output "data_source_result" {
+  value = data.local_file.file_reference.content_base64
+}
+
+data "local_file" "file_reference" {
+  filename = var.heroes_filename
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,2 @@
-heroes_content = "Batman; SuperMan; Flash; IronMan; SpiderMan; AquaMan"
+heroes_content = "Batman,SuperMan,Flash,IronMan,SpiderMan,AquaMan,Thor"
+heroes_filename = "./tmp/heroes.txt"
